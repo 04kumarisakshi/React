675921e6c61cbe67cda733f1f9d32c1032384794
kumari sakshi <sakshi.kumari80102@gmail.com>
2023-10-11 23:21:55 +0530

    test02

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <> <p> your name from the moon</p>
+    <p>Caught in the void, I ma be home soon</p>
+   <p>So we can do the impossible</p>
+   <p>She got a walk that ll stop the room</p>
+   <p>
+   She got in my car, so invincible
+   </p>
+   <p>So we can do the impossible</p>
+   
+   </>
   )
 }
 
