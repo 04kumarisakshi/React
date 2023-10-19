5a26bbc07baa3feb94a14f9a8c310d0896f40537
kumari sakshi <sakshi.kumari80102@gmail.com>
2023-10-20 00:01:04 +0530

    Router

---
+++ ReactRouter/src/main.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './components/Home/home.jsx'
+import About from './components/About/About.jsx'
+import Contact from './components/ContactUs/Contact.jsx'
+
+const router =createBrowserRouter([{
+  path:'/',
+  element:<App/>,
+  children:[
+    {
+      path:"",
+      element:<Home/>
+
+    },
+    {path:"about",
+    element: <About/>
+},
+{path:"contactus",
+element:<Contact/>
+}
+  ]
+}
+])
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
