fc7abf68fbed47e2701e63d25cf18ba9dae12b17
kumari sakshi <sakshi.kumari80102@gmail.com>
2023-10-24 22:35:56 +0530

    context Api

---
+++ 01Context/src/Context/UserContext.js
@@ -0,0 +1,13 @@
+ import React from "react";
+
+ const UserContext=React.createContext()
+
+ export default UserContext
+
+//  Provider
+
+{/* <UserContext>
+    <Card>
+    <Data/>
+    </Card>
+    </UserContext> */}
